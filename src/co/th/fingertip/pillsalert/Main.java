86afa16a5884e3c3770c0340d117dcadeb03ef6c
kittinunf <kittinun.f@gmail.com>
2011-07-12 14:36:32 +0700

    Rename package

---
+++ src/co/th/fingertip/pillsalert/Main.java
@@ -0,0 +1,16 @@
+package co.th.fingertip.pillsalert;
+
+import co.th.fingerprint.pillsalert.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+}
