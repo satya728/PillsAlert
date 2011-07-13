68f4301edb8831ec076ab46b57e99b0ee2d8d985
Warut Surapat <s.warut@gmail.com>
2011-07-13 12:21:39 +0700

    update main.java

--- src/th/co/fingertip/pillsalert/Main.java
+++ src/th/co/fingertip/pillsalert/Main.java
@@ -1,8 +1,13 @@
 package th.co.fingertip.pillsalert;
 
 import th.co.fingertip.pillsalert.R;
+import th.co.fingertip.pillsalert.camera.CameraTestActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Main extends Activity {
 
@@ -12,5 +17,25 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()){
+			case R.id.camera_test_activity:
+				Intent intent = new Intent(this, CameraTestActivity.class);
+				startActivity(intent);
+				break;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
 	
 }
