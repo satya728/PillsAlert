4650a55e548a7f9fca12a6d0e510482e15aa1c7b
kittinunf <kittinun.f@gmail.com>
2011-08-18 20:48:47 +0700

    Fix dummy thing

--- src/th/co/fingertip/pillsalert/adapter/ImageSpinnerAdapter.java
+++ src/th/co/fingertip/pillsalert/adapter/ImageSpinnerAdapter.java
@@ -7,6 +7,7 @@ import th.co.fingertip.pillsalert.PillsAlertEnum;
 import th.co.fingertip.pillsalert.R;
 import th.co.fingertip.pillsalert.db.DatabaseConfiguration;
 import th.co.fingertip.pillsalert.factory.ImageFactory;
+import th.co.fingertip.pillsalert.util.Util;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.database.Cursor;
@@ -22,12 +23,13 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 	private Context context;
 	int gallery_item_background;
 	private boolean adjustable_flag;
+	private boolean notification_dummy;
 	private Cursor cursor;
 	private int model;
-	private boolean dummy_needed;
 	
 	//private Vector<Integer> image_ids = new Vector<Integer> (Arrays.asList(int_values));
 	private Vector<String> images = new Vector<String>();
+	private Vector<Boolean> pill_dummy_flags = new Vector<Boolean>(); //for pills
 	private Vector<Long> ids = new Vector<Long>();
 	//private Vector<Integer> image_id = new Vector<Integer>();
 	
@@ -45,11 +47,17 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 		adjustable_flag = flag;
 		this.cursor = cursor;
 		this.model = model;
+		notification_dummy = false;
 		fillData();
+		if (cursor.getCount() == 0 && model == PillsAlertEnum.Model.NOTIFICATION) {
+			notification_dummy = true;
+			ids.add((long) 0);
+			images.add("");
+		}
+		
 	}
 	
 	private void fillData() {
-		// TODO Auto-generated method stub
 		if (!cursor.isClosed()) {
 			cursor.move(-1);
 		} 
@@ -59,6 +67,8 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 			switch(model) {
 			case PillsAlertEnum.Model.PILL:
 				id = cursor.getInt(cursor.getColumnIndex(DatabaseConfiguration.PILL_SCHEMA_KEYS[0]));
+				pill_dummy_flags.add((Util.integer_to_boolean(cursor.getInt(
+						cursor.getColumnIndex(DatabaseConfiguration.PILL_SCHEMA_KEYS[4])))));
 				break;
 			case PillsAlertEnum.Model.NOTIFICATION:
 				id = cursor.getInt(cursor.getColumnIndex(DatabaseConfiguration.NOTIFICATION_SCHEMA_KEYS[1]));
@@ -76,7 +86,6 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 		gallery_item_background = a.getResourceId(
 				R.styleable.Gallery1_android_galleryItemBackground, 0);
 		a.recycle();
-		dummy_needed = true;
 	}
 	
 	@Override
@@ -120,11 +129,20 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 		i.setLayoutParams(new Gallery.LayoutParams(150, 150));
 		i.setScaleType(ImageView.ScaleType.FIT_XY);
 		i.setBackgroundResource(gallery_item_background);
-		if (dummy_needed) {
-			i.setImageResource(R.drawable.dummy_pill);
+		if (model == PillsAlertEnum.Model.NOTIFICATION) {
+			if (notification_dummy) {
+				i.setImageResource(R.drawable.dummy_pill);
+			} else {
+				i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
+			}
 		} else {
-			i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
+			if (pill_dummy_flags.get(position)) {
+				i.setImageResource(R.drawable.dummy_pill);
+			} else {
+				i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
+			}
 		}
+
 		i.setTag(ids.get(position));
 		
 		return i;
