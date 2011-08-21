5d70c655cae816c99fed45618dbef4f857d7dec9
kittinunf <kittinun.f@gmail.com>
2011-08-21 16:17:04 +0700

    add new added files

--- src/th/co/fingertip/pillsalert/adapter/ImageSpinnerAdapter.java
+++ src/th/co/fingertip/pillsalert/adapter/ImageSpinnerAdapter.java
@@ -1,6 +1,5 @@
 package th.co.fingertip.pillsalert.adapter;
 
-import java.util.Arrays;
 import java.util.Vector;
 
 import th.co.fingertip.pillsalert.PillsAlertEnum;
@@ -19,17 +18,16 @@ import android.widget.ImageView;
 
 public class ImageSpinnerAdapter extends BaseAdapter {
 
-
+	private String DUMMY = "dummy_pill.PNG";
+	
 	private Context context;
 	int gallery_item_background;
 	private boolean adjustable_flag;
-	private boolean notification_dummy;
 	private Cursor cursor;
 	private int model;
-	
+
 	//private Vector<Integer> image_ids = new Vector<Integer> (Arrays.asList(int_values));
 	private Vector<String> images = new Vector<String>();
-	private Vector<Boolean> pill_dummy_flags = new Vector<Boolean>(); //for pills
 	private Vector<Long> ids = new Vector<Long>();
 	//private Vector<Integer> image_id = new Vector<Integer>();
 	
@@ -47,38 +45,29 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 		adjustable_flag = flag;
 		this.cursor = cursor;
 		this.model = model;
-		notification_dummy = false;
-		if (cursor.getCount() == 0 && model == PillsAlertEnum.Model.NOTIFICATION) {
-			notification_dummy = true;
-			ids.add((long) 0);
-			images.add("");
-		}
 		fillData();
 	}
 	
 	private void fillData() {
-		if(cursor.getCount()!=0){
-			if (!cursor.isClosed()) {
-				cursor.move(-1);
-			} 
-			while(cursor.moveToNext()) {
-				long id = -1;
-				
+		if(cursor.getCount()!= 0){
+			
+			for (cursor.move(-1);cursor.moveToNext();) {
+				long id = -1;	
 				switch(model) {
 				case PillsAlertEnum.Model.PILL:
 					id = cursor.getInt(cursor.getColumnIndex(DatabaseConfiguration.PILL_SCHEMA_KEYS[0]));
-					pill_dummy_flags.add((Util.integer_to_boolean(cursor.getInt(
-							cursor.getColumnIndex(DatabaseConfiguration.PILL_SCHEMA_KEYS[3])))));
 					break;
 				case PillsAlertEnum.Model.NOTIFICATION:
 					id = cursor.getInt(cursor.getColumnIndex(DatabaseConfiguration.NOTIFICATION_SCHEMA_KEYS[1]));
 					break;
 				}
 				ids.add(id);
-				String image_name = Long.toString(id) + ".PNG";
+				String image_name = cursor.getString(cursor.getColumnIndex
+						(DatabaseConfiguration.PILL_SCHEMA_KEYS[3]));
 				images.add(image_name);
 			}
 		}
+		cursor.close();
 	}
 
 	private void init(Context context) {
@@ -92,7 +81,7 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		
-		return ids.size();
+		return images.size();
 	}
 
 	@Override
@@ -108,15 +97,13 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 	}
 	
 	public boolean addItem(Object object_info) {
-		
-		String pic_name = object_info.toString() + ".PNG";
-		
+		String file_name = object_info.toString();
 		if (!adjustable_flag) {
 			return false;
 		}
 		
-		if (!images.contains(pic_name)) {
-			images.add(pic_name);
+		if (!images.contains(file_name)) {
+			images.add(file_name);
 		} else {
 			return false;
 		}
@@ -130,22 +117,13 @@ public class ImageSpinnerAdapter extends BaseAdapter {
 		i.setLayoutParams(new Gallery.LayoutParams(150, 150));
 		i.setScaleType(ImageView.ScaleType.FIT_XY);
 		i.setBackgroundResource(gallery_item_background);
-		if (model == PillsAlertEnum.Model.NOTIFICATION) {
-			if (notification_dummy) {
-				i.setImageResource(R.drawable.dummy_pill);
-			} else {
-				i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
-			}
+		String file_name = images.get(position);
+		if (file_name.equals(DUMMY)) {
+			i.setImageResource(R.drawable.dummy_pill);
 		} else {
-			if (!pill_dummy_flags.get(position)) {
-				i.setImageResource(R.drawable.dummy_pill);
-			} else {
-				i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
-			}
+			i.setImageBitmap(ImageFactory.get_bitmap(images.get(position)));
 		}
-
-		i.setTag(ids.get(position));
-		
+		i.setTag(file_name);
 		return i;
 	}
 	
