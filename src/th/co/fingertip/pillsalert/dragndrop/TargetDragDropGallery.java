21a9c9a83b6f1fa4e84baf998a27f0712c01fb1d
Warut Surapat <s.warut@gmail.com>
2011-09-03 18:41:02 +0700

    pon update >_<)

--- src/th/co/fingertip/pillsalert/dragndrop/TargetDragDropGallery.java
+++ src/th/co/fingertip/pillsalert/dragndrop/TargetDragDropGallery.java
@@ -85,7 +85,7 @@ public class TargetDragDropGallery extends Gallery implements
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 
--- src/th/co/fingertip/pillsalert/ui/PillPeriodActivity.java
+++ src/th/co/fingertip/pillsalert/ui/PillPeriodActivity.java
@@ -10,20 +10,24 @@ import th.co.fingertip.pillsalert.adapter.PillImageSpinnerAdapter;
 import th.co.fingertip.pillsalert.db.Notification;
 import th.co.fingertip.pillsalert.db.Period;
 import th.co.fingertip.pillsalert.db.Pill;
-import th.co.fingertip.pillsalert.dragndrop.DragDropGallery;
 import th.co.fingertip.pillsalert.dragndrop.DragLayer;
 import th.co.fingertip.pillsalert.dragndrop.SourceDragDropGallery;
 import th.co.fingertip.pillsalert.dragndrop.TargetDragDropGallery;
 import th.co.fingertip.pillsalert.util.Util;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 
-public class PillPeriodActivity extends Activity implements OnClickListener {
+public class PillPeriodActivity extends Activity implements OnClickListener, OnItemClickListener, OnItemLongClickListener {
 
 	
 	private SourceDragDropGallery pill_gallery;
@@ -93,19 +97,25 @@ public class PillPeriodActivity extends Activity implements OnClickListener {
 		pill_gallery.setDragger(drag_layer);
 		
 		previous_button.setOnClickListener(this);
-		next_button.setOnClickListener(this);	
+		next_button.setOnClickListener(this);
+		
+		pill_gallery.setOnItemClickListener(this);
+		period_gallery.setOnItemClickListener(this);
+		
+		period_gallery.setOnItemLongClickListener(this);
 	}
 
 	
 	private void updateNotification(NotificationImageSpinnerAdapter adapter){
 		Period current_period = periods[period_index];
-		Notification[] current_period_notification = Notification.find("period_id = ?", new String[]{current_period+""});
+		Notification[] current_period_notification = Notification.find("period_id = ?", new String[]{current_period.id+""});
 		
 		for(int i=0; i<current_period_notification.length; i++){
+			int uid =current_period_notification[i].id;
 			Notification.delete(current_period_notification[i].id);
 		}
 		
-		for(int j=0; j<adapter.pill_ids.size(); j++){
+		for(int j=0; j<adapter.getCount(); j++){
 			Pill current_pill = Pill.find(adapter.pill_ids.get(j));
 			Notification new_notification = new Notification( current_pill.id, current_period.id, current_pill.image);
 			new_notification.save();
@@ -129,6 +139,7 @@ public class PillPeriodActivity extends Activity implements OnClickListener {
 		super.onBackPressed();
 	}
 
+	
 	@Override
 	public void onClick(View view) {
 		
@@ -148,6 +159,7 @@ public class PillPeriodActivity extends Activity implements OnClickListener {
 				}
 				catch(ArrayIndexOutOfBoundsException e){
 					Util.put(getApplicationContext(), "no previous period", Util.SHORT_TRACE);
+					period_index = 0;
 				}
 				break;
 			
@@ -162,9 +174,34 @@ public class PillPeriodActivity extends Activity implements OnClickListener {
 				}
 				catch(ArrayIndexOutOfBoundsException e){
 					Util.put(getApplicationContext(), "no next period", Util.SHORT_TRACE);
+					period_index = periods.length - 1;
 				}
 				break;
 		}
+	}
+
+	
+	@Override
+	public void onItemClick(AdapterView<?> adapter_view, View view, int position, long id) {
+		int pill_id = (Integer)view.getTag(R.id.image_id);
+		Bundle pill_bundle = new Bundle();
+		Pill pill = Pill.find(pill_id);
+		pill_bundle.putInt(Pill.ID, pill.id);
+		pill_bundle.putString(Pill.TITLE, pill.title);
+		pill_bundle.putString(Pill.NOTE, pill.note);
+		pill_bundle.putString(Pill.IMAGE, pill.image);
+		pill_bundle.putInt("request_code",  PillsAlertEnum.Request.PILL_READ);
+		Intent view_pill_intent = new Intent(getApplicationContext(), PillEditorActivity.class);
+		view_pill_intent.putExtras(pill_bundle);
+		startActivityForResult(view_pill_intent, PillsAlertEnum.Request.PILL_READ);
+	}
+
+
+	@Override
+	public boolean onItemLongClick(AdapterView<?> adapter_view, View view, int position, long id) {
+		int pill_id = (Integer)view.getTag(R.id.image_id);
+		Pill.delete(pill_id);
+		return true;
 	}	
 }
 	
