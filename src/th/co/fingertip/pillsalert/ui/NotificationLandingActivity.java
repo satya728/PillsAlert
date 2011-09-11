6cc73c07fce46d86fb387b4d954f24bec4ba1e41
Warut Surapat <s.warut@gmail.com>
2011-09-11 18:06:08 +0700

    update from pon

--- src/th/co/fingertip/pillsalert/ui/NotificationLandingActivity.java
+++ src/th/co/fingertip/pillsalert/ui/NotificationLandingActivity.java
@@ -9,16 +9,21 @@ import th.co.fingertip.pillsalert.factory.ImageFactory;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.Time;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager.LayoutParams;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
-public class NotificationLandingActivity extends Activity{//extends ListActivity
+public class NotificationLandingActivity extends Activity{//
 
 	private Pill[] pills;
 	private int period_id;
@@ -31,71 +36,47 @@ public class NotificationLandingActivity extends Activity{//extends ListActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.notification_landing);
+		ListView list_view = new ListView(this);
+		setContentView(list_view);
+		
 		Notification.init(this);
 		Pill.init(this);
 		Period.init(this);
-		//registerForContextMenu(getListView());
-		
-//		Period[] periods = Period.find(Period.ALL);
-//		period_id = periods[0].id;
-		
-		period_id = savedInstanceState.getInt("period_id");
-		
-		
-		
-		pills = Notification.findPillByPeriod(period_id);
-		
-		LinearLayout ll = new LinearLayout(this);
-		
-		for(int i = 0 ; i < pills.length ; i++){
-			LinearLayout row = new LinearLayout(this);
-			//row.setLayoutParams(new La)
-			LinearLayout.LayoutParams l_params = 
-				new LinearLayout.LayoutParams(
-						LinearLayout.LayoutParams.MATCH_PARENT,
-						LinearLayout.LayoutParams.WRAP_CONTENT
-				);
-			
-			//row.setLayoutParams(l_params);			
-			TextView tv = new TextView(this);
-			tv.setText(pills[i].title);
-			ImageView iv = new ImageView(this);
-			
-			if (pills[i].image.equals(PillsAlertEnum.FileName.PILL_DUMMY_FILENAME)) {
-				iv.setImageResource(R.drawable.dummy_pill);
-			} else {
-				iv.setImageBitmap(ImageFactory.get_bitmap(pills[i].image));
+
+		period_id = Period.find(Period.ALL)[0].id;//savedInstanceState.getInt("period_id");
+		Pill[] pills = Notification.findPillByPeriod(period_id);
+		CustomAdapter c = new CustomAdapter(this, R.layout.notification_landing_row, R.id.notification_landing_pill_title, pills);
+	}
+	
+	private class CustomAdapter extends ArrayAdapter<Pill>{
+
+		public CustomAdapter(Context context, int resource, int textViewResourceId, Pill[] objects) {
+			super(context, resource, textViewResourceId, objects);
+
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			if(row == null){
+				row = getLayoutInflater().inflate(R.layout.notification_landing_row, parent, false);
 			}
 			
-			row.addView(iv);
-			row.addView(tv);
-			ll.addView(row);
+			Pill pill = getItem(position);
+			ImageView pill_image = (ImageView)row.findViewById(R.id.notification_landing_pill_image);
+			TextView pill_title = (TextView)row.findViewById(R.id.notification_landing_pill_title);
+			
+			pill_title.setText(pill.title);
+			if (pill.image.equals(PillsAlertEnum.FileName.PILL_DUMMY_FILENAME)) {
+				pill_image.setImageResource(R.drawable.dummy_pill);
+			} else {
+				pill_image.setImageBitmap(ImageFactory.get_bitmap(pill.image));
+			}
+			
+			return row;
 		}
-		
-		setContentView(ll);
-		
-		//fill_data();
 		
 		
 	}
 
-
-//	private void fill_data() {
-//		//periods = Period.find(Period.ALL);
-//		Cursor period_cursor = Period.find_cursor(Period.ALL);
-//		period_cursor.moveToFirst();
-//		startManagingCursor(period_cursor);
-//		String[] from_column = {Period.TITLE};
-//		int[] to_view = {R.id.notification_landing_row_item};
-//		SimpleCursorAdapter period_cursor_adapter = new SimpleCursorAdapter(
-//			this, 
-//			R.layout.period_setting_row, 
-//			period_cursor, 
-//			from_column, 
-//			to_view
-//		);
-//		setListAdapter(period_cursor_adapter);
-//	}
-	
 }
